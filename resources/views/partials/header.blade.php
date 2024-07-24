f5172d8a4bf4baa0e2bfc7e61156942871362f96
ibnu <ibnuhamdani234@gmail.com>
2024-07-24 12:20:23 +0700

    update logo and title

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <head>
 
     <meta charset="utf-8" />
-    <title>Dashboard | Adminto - Responsive Admin Dashboard Template</title>
+    <title>Aplikasi Penyimpanan Dokumen Barang Milik Negara</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
     <meta content="Coderthemes" name="author" />
